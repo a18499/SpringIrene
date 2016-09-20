22d449cde4aa4447b71fe1dd55af52c68431699f
peter <a184999@gmail.com>
2016-09-20 21:07:44 +0800

    [Update] index with bootstrap

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -15,7 +15,7 @@ public class GreetingController {
     }
     @RequestMapping("/helloworld")
     public String helloworld(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+        model.addAttribute("name", name);       
         return "helloworld";
     }
 
